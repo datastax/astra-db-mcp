9b1b2583a547a071ef434be9af5b305aec140688
Phil Miesle <mieslep@users.noreply.github.com>
2025-06-25 16:09:18 +0100

    allow configurable keyspace (#15)

--- util/db.ts
+++ util/db.ts
@@ -16,6 +16,10 @@ import "dotenv/config";
 import { DataAPIClient } from "@datastax/astra-db-ts";
 
 const client = new DataAPIClient(process.env.ASTRA_DB_APPLICATION_TOKEN);
-export const db = client.db(
-  process.env.ASTRA_DB_API_ENDPOINT || "https://api.astra.datastax.com"
-);
+
+const endpoint = process.env.ASTRA_DB_API_ENDPOINT || "https://api.astra.datastax.com";
+const keyspace = process.env.ASTRA_DB_KEYSPACE;
+
+const dbOptions = keyspace ? { keyspace: keyspace } : undefined;
+
+export const db = client.db(endpoint, dbOptions);
